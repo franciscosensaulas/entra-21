385b52a2a96330d3804cd789580b01e455083cf3
Francisco Lucas Sens <franciscosensaulas@gmail.com>
2022-05-06 20:14:35 -0300

    Criado exercício 1 Conforme solicitado pelo professor foi criado o exercício 01 e adicionado ao menu

---
+++ Entra21.ExemplosWhile/Exercicio01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosWhile
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+
+        }
+    }
+}
--- Entra21.ExemplosWhile/Program.cs
+++ Entra21.ExemplosWhile/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("MEU omelete é lindinho");
+﻿using Entra21.ExemplosWhile;
+
+Console.Write(@"------Arquivos-------
+1 - Exercício 01");
+int exercicioUsuario = Convert.ToInt32(Console.ReadLine());
+
+if(exercicioUsuario == 1)
+{
+    Exercicio01 exercico01 = new Exercicio01();
+    exercico01.Executar();
+}
